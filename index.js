9414bc1f691add9777cda9bdbfea89904a2a4219
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-05-03 14:01:08 +0600

    send chef recipe data on client side

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+const chefData = require("./data/chef-recipe.json")
+
+app.use(cors());
 
 app.get('/' , (req , res) =>{
     res.send("Thai cooking recipe in running")
+})
+
+app.get('/chef-data' , (req , res) => {
+    res.send(chefData)
 })
 
 app.listen( port , () =>{
